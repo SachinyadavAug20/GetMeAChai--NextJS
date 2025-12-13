8702e7af4284188c6df22584f7acba2b762919ac
SachinyadavAug20 <samtagon777@gmail.com>
2025-12-13 22:41:58 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <div className="bg-black text-white w-full p-5 text-center text-xl">
+        Hello chai
+    </div>
+  </div>
+  
+ );
+}
